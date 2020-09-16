92bf2d54aecb2b0733d31519ec1cf725bf63cf3b
Tamasjoo <tamasjoo89@gmail.com>
2020-09-16 11:55:26 +0200

    added basic html and css

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="main">
+            <nav>
+                <button>Show All</button>
+                <button>Browse</button>
+                <button>Upload</button>
+            </nav>
+            <main>
+                <header>
+                    <div>Name</div>
+                    <div>Last Modified On</div>
+                    <div>Size</div>
+                </header>
+                <div className="results_container">
+                    <img src="/img/pdf_logo.png" alt="file extension logo" />
+                    <p>File Name</p>
+                    <p>14.09.2020</p>
+                    <p>5 MB</p>
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default App;
