5a2c517dc5fc1e71fa5b29e971d4b8cdd5047533
Tamasjoo <tamasjoo89@gmail.com>
2020-09-25 16:58:07 +0200

    added bootstrap, removed css, started adding some styling with bootstrap

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,36 +1,50 @@
 import React, { useState, useEffect } from "react";
-import "./App.css";
+
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Browse from "./Browse";
 import ShowAll from "./ShowAll";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Col, Row } from "react-bootstrap";
 
 const App = () => {
     const [nav, setNav] = useState(""); // if we don´t want to hide the nav at some point we can also remove this because then probably we don´t want to change its state
 
     const [browseDocuments, setBrowseDocuments] = useState("");
     return (
-        <BrowserRouter>
-            <div className="main_container">
-                <nav>
-                    <Link to="/">
-                        <button>Show All</button>
-                    </Link>
-                    <Link to="/browse">
-                        <button>Browse</button>
-                    </Link>
-                    <button>Upload</button>
-                </nav>
-                <main>
-                    <header>
-                        <div id="header_name">Name</div>
-                        <div id="header_last_modified">Last Modified On</div>
-                        <div id="header_size">Size</div>
-                    </header>
-                    <Route exact path="/" component={ShowAll} />
-                    <Route path="/browse" component={Browse} />
-                </main>
-            </div>
-        </BrowserRouter>
+        <Container>
+            <BrowserRouter>
+                <Row>
+                    <Col className="bg-secondary" lg={2}>
+                        <nav>
+                            <Link to="/">
+                                <Row>
+                                    <button>Show All</button>
+                                </Row>
+                            </Link>
+                            <Link to="/browse">
+                                <Row>
+                                    <button>Browse</button>
+                                </Row>
+                            </Link>
+                            <Row>
+                                <button>Upload</button>
+                            </Row>
+                        </nav>
+                    </Col>
+                    <Col>
+                        <Row>
+                            <div id="header_name">Name</div>
+                            <div id="header_last_modified">
+                                Last Modified On
+                            </div>
+                            <div id="header_size">Size</div>
+                            <Route exact path="/" component={ShowAll} />
+                            <Route path="/browse" component={Browse} />
+                        </Row>
+                    </Col>
+                </Row>
+            </BrowserRouter>
+        </Container>
     );
 };
 
