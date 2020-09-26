070f665d38efe3d21bfdda0495a73294344e4f36
David Sieberger <d.sieberger@gmail.com>
2020-09-27 00:19:19 +0200

    Removed Bootstrap NPM modules. Added CDN bootstrap CSS. Reajusted view.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,49 +3,45 @@ import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 import Browse from "./Browse";
 import ShowAll from "./ShowAll";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Col, Row } from "react-bootstrap";
 
 const App = () => {
-    const [nav, setNav] = useState(""); // if we don´t want to hide the nav at some point we can also remove this because then probably we don´t want to change its state
+  const [nav, setNav] = useState(""); // if we don´t want to hide the nav at some point we can also remove this because then probably we don´t want to change its state
 
-    const [browseDocuments, setBrowseDocuments] = useState("");
-    return (
-        <Container>
-            <BrowserRouter>
-                <Row>
-                    <Col className="bg-secondary" lg={2}>
-                        <nav>
-                            <Link to="/">
-                                <Row>
-                                    <button>Show All</button>
-                                </Row>
-                            </Link>
-                            <Link to="/browse">
-                                <Row>
-                                    <button>Browse</button>
-                                </Row>
-                            </Link>
-                            <Row>
-                                <button>Upload</button>
-                            </Row>
-                        </nav>
-                    </Col>
-                    <Col>
-                        <Row>
-                            <div id="header_name">Name</div>
-                            <div id="header_last_modified">
-                                Last Modified On
-                            </div>
-                            <div id="header_size">Size</div>
-                            <Route exact path="/" component={ShowAll} />
-                            <Route path="/browse" component={Browse} />
-                        </Row>
-                    </Col>
-                </Row>
-            </BrowserRouter>
-        </Container>
-    );
+  const [browseDocuments, setBrowseDocuments] = useState("");
+  return (
+    <BrowserRouter>
+      <div className="container">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">
+            <img src="/img/bk.jpg" width="30" height="30" alt="" loading="lazy" />
+          </a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse"
+            data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup"
+            aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link active" to="/">Show All <span className="sr-only">(current)</span></Link>
+              <Link className="nav-link" to="/browse">Browse</Link>
+              <Link className="nav-link" to="#">Upload</Link>
+            </div>
+          </div>
+        </nav>
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Name</th>
+              <th scope="col">Created At</th>
+              <th scope="col">Size</th>
+            </tr>
+          </thead>
+          <Route exact path="/" component={ShowAll} />
+          <Route path="/browse" component={Browse} />
+        </table>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- client/src/ShowAll.tsx
+++ client/src/ShowAll.tsx
@@ -3,40 +3,41 @@ import "./results.css";
 import axios from "./axiosConfig";
 
 const ShowAll = () => {
-    const [allDocuments, setAllDocuments] = useState([]);
+  const [allDocuments, setAllDocuments] = useState([]);
 
-    useEffect(() => {
-        axios.get("/api/documents").then((res) => {
-            console.log("res.data is: ", res.data);
-            setAllDocuments(res.data);
-        });
-    }, []);
+  useEffect(() => {
+    axios.get("/api/documents").then((res) => {
+      console.log("res.data is: ", res.data);
+      setAllDocuments(res.data);
+    });
+  }, []);
 
-    const download = (name: any) => {
-        console.log("document name clicked", name);
-        axios
-            .get("/api/documents/" + name, {
-                responseType: "stream",
-            })
-            .then((res) => {
-                console.log(res);
-            });
-    };
+  const download = (name: any) => {
+    console.log("document name clicked", name);
+    axios
+    .get("/api/documents/" + name, {
+      responseType: "stream",
+    })
+    .then((res) => {
+      console.log(res);
+    });
+  };
 
-    return (
-        <div className="results_container">
-            {!allDocuments && <p>No Files</p>}
-            {allDocuments &&
-                allDocuments.map((document: any, i: any) => {
-                    return (
-                        <div key={i} onClick={(e) => download(document.name)}>
-                            {document.name} {document.timeCreated}{" "}
-                            {document.size}
-                        </div>
-                    );
-                })}
-        </div>
+  return (
+    <tbody>
+    {!allDocuments && <p>No Files</p>}
+    {allDocuments &&
+      allDocuments.map((document: any, i: any) => {
+        return (
+          <tr key={i} onClick={(e) => download(document.name)}>
+            <td>{document.name}</td>
+            <td>{document.timeCreated}</td>
+            <td>{document.size}</td>
+          </tr>
+        );
+      })}
+    </tbody>
     );
-};
+  };
 
-export default ShowAll;
+  export default ShowAll;
